0dde981b420863db76c1d73313ec36f240bc0b42
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-07-19 08:45:25 -0700

    first commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
     # Create a new matrix with the same size as the input matrix
-    new_matrix = [[num**2 for num in row] for row in matrix]
+    new_matrix = [[number**2 for number in row] for row in matrix]
     return new_matrix
-
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,10 @@
 
 def search_replace(my_list, search, replace):
     # Create a new list with the replaced elements
-    new_list = [replace if num == search else num for num in my_list]
+    new_list = []
+    for item in my_list:
+        if item != search:
+            new_list.append(item)
+        else:
+            new_list.append(replace)
     return new_list
-
